119f7cc736157085ead9924cface0bde73227af2
Maxim <maximsheleh@gmail.com>
2021-08-28 16:15:36 +0300

    fix fstring response

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ try:
         path = received_data.split(" ")[1]
         document = URL.parse(path)
         response = f"HTTP/1.1 200 OK\nContent-Type: text/html; charset=utf-8" \
-                    "\n\n{document}"
+                    f"\n\n{document}"
 
         client_socket.send(response.encode("utf-8"))
         client_socket.shutdown(socket.SHUT_RDWR)
